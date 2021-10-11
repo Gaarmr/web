bb612ba8b080da6ae67ca6e81af86137adaf5fcf
Gaarmr <vdnv@bk.ru>
2021-10-12 01:17:30 +0600

    weather and debug

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from weather import weather_by_city
+
 app = Flask(__name__)
 
 @app.route("/")
 
-def hello():
-    return "Привет!"
+def index():
+    weather = weather_by_city('Nur-Sultan')
+    #weather = False
+    if weather:
+        return f"Температура: {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
+    else:
+        return 'Сервис временно недоступен'
+
+if __name__=="__main__":
+    app.run(debug=True)

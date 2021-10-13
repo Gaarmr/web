b83a95d97d31f58dbcbc006aeb1826edd5bb9bd5
Gaarmr <vdnv@bk.ru>
2021-10-13 08:53:33 +0600

    webapp

---
+++ webapp/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from python_news import get_python_news
+from weather import weather_by_city
+
+def create_app():
+
+    app = Flask(__name__)
+
+    @app.route("/")
+
+    def index():
+        title = 'Новости Python'
+        weather = weather_by_city('Nur-Sultan')
+        news = get_python_news()
+        return render_template('index.html', page_title=title, weather_text=weather, news_list=news)
+    
+    return app
